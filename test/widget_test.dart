bd3a6b7c56d417b155128bfa4b3d1178ade92f9e
Aaysha Mohamed <118212755+AAYSHAA@users.noreply.github.com>
2024-03-24 17:11:00 +0530

    Add files via upload
    AR Model Integration Flutter pages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:imagine_cup_ui/main.dart';
+import 'package:untitled/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Socar());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
